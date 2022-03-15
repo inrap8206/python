bf5a97fecb11b95f8192c01234c690d60b7cf006
Woocheol Cho <59816572+inrap8206@users.noreply.github.com>
2022-03-15 21:58:31 +0900

    Update 04_for_while.py

--- 04_for_while.py
+++ 04_for_while.py
@@ -1,63 +1,103 @@
-#for 문을 사용하여 딕셔너리 타입의 d의 모든 value를 출력해 보시오.
-d= {'yoon': 1, 'park': 2, 'kim': 10}
-for i in d:
-    print(d[i], end=' ')
+#단어(문자열)가 주어질 때, 아래와 같이 출력되도록 작성하시오.
+#while문을 이용하여 작성(문자열)
+word = "Python"
+
+i = 0
+while i < len(word):
+    print(word[i])
+    i += 1
+
+    
+#아래와 같이 출력되는 프로그램을 작성하시오.
+for i in range(10,0,-1):
+    print(i, end=',')
+print('Happy new year!!')
+
+
+#양의 두 정수 a, b를 입력 받아, a부터 b까지의 정수의 합을 구하여 출력하는 프로그램을 작성하시오.
+#(for 또는 while을 이용할 것) 단, 조건 a <= b 을 만족하는 값만 고려한다.
+nums = input("Enter two integers: ")
+a, b = map(int, nums.split())
+ab_sum = 0
+for i in range(a, b+1):
+    ab_sum += i
+print("The sum from %d to %d is %d" % (a, b, ab_sum))
+
+
+#주어진 문자열에 문자a가 몇 개 있는지 구하는 프로그램을 작성하시오.(for문 사용할 것)
+word = 'banana'
+n = 0
+for c in word:
+    if c == 'a':
+        n += 1
+print(n)
+
+
+#for 문과 range()함수를 이용하여 다음과 같이 출력되도록 작성하시오. (각 줄마다 range()함수 사용)
+for i in range(0,10):
+    print(i, end=' ')
+print()
+    
+for i in range(0,51,5):
+    print(i, end=' ')
+print()
+
+for i in range(10,0,-1):
+    print(i, end=' ')
+print()
+
+
+#for 문을 사용하여 리스트(colors)의 모든 내용을 출력하시오. (단, range()를 이용하지 않는다.)
+colors = ["red", "green", "blue"]
+for i in colors:
+    print(i)
     
     
-#아래와 같은 딕셔너리가 있다.
-dic1 = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5}
-key_ls = list(dic1.keys())
-key_ls.sort()
-for i in key_ls:
-    print("%s %d" % (i, dic1[i]))
-    
-    
-#어떤 문장을 입력 받으면 해당 문장에서 각 문자가 몇 개씩 나오는지 저장하는 딕셔너리를 만든 후, 아래와 같이 출력하시오.
-a = input("Enter a sentence:")
-d = {}
+#리스트 a 전체를 반복해서 방문하되, 짝수만 출력하시오.(for 문 사용)
+a = [1,3,4,5,6,7,8,9,10,11,12,13]
 for i in a:
-    d[i] = a.count(i)
-print(d)
+    if i % 2 == 0:
+        print(i, end=' ')
+        
+        
+#구구단 출력1
+a = int(input("출력하고 싶은 단을 입력하세요:"))
+for i in range(1, 10):
+    print("%d * %d = %2d" % (a, i, a*i))
+    
+    
+#구구단 출력2
+for n in range(2,10):
+    print("== %d단 ==" % n)
+    for i in range(1, 10):
+        print("%d * %d = %2d" % (n, i, n*i))
+        
+        
+#구구단 출력2 (중첩 반복문)
+i, k, guguline = 0, 0, ""
+for i in range(1, 10):
+    guguline = ""
+    for k in range(2, 10):
+        guguline = guguline + str("%d * %d = %2d  " % (k, i, k*i))
+    print(guguline)
+    
+    
+#★ 출력 프로그램
+num = input("Input an integer: ")
+for i in num: 
+    print("★"*int(i))
+    
+    
+#“done“을 입력할 때까지 사용자로부터 숫자를 입력받아 리스트에 저장하고,
+#“done“을 입력하면, 리스트의 평균, 최대값과 최소값을 출력하는 프로그램을 작성하시오.
+num = input("Enter a number:")
+num_ls = []
+while num != "done":
+    num_ls.append(float(num))
+    num = input("Enter a number:")
 
-
-#양의 정수 a와 b를 받아 a부터 b까지 곱한 값을 반환하는 mult 함수를 작성하시오.
-def mult(a,b):
-    result = 1
-    for i in range(a, b+1):
-        result *= i
-    return result
-
-print(mult(1,3))
-print(mult(2,5))
-
-
-#1개 이상의 2차원 벡터들을 받아, 벡터들의 합을 구하여 반환하는 함수를 작성하시오 (적어도 1개는 반드시 입력되어야함).
-def vector_sum(vector, *vectors):
-    vector1 = list(vector)
-    for vector_ in vectors:
-        vector1[0] += vector_[0]
-        vector1[1] += vector_[1]
-    return vector1
-
-
-v1=[0, 1]
-v2=[0.5, 0.5]
-v3=[1, 0]
-v4=[6, 4]
-v5=[3.14, 2.72]
-m1=vector_sum(v1,v2,v3)
-m2=vector_sum(v1,v2,v3,v4)
-m3=vector_sum(v3,v5)
-print(m1,m2,m3)
-
-
-#로또 번호 자동 기입 프로그램을 작성하시오
-#(5개의 로또 번호 자동 기입 결과 작성. 각 로또 번호 자동 기입 결과는 1~45의 숫자 중 6개를 임의로 선택하여
-#정렬한 후 출력되어야 하며, 각 자동 기입에서 중복되는 숫자가 있어서는 안된다.)
-import random
-nums = list(range(1,46))
-print("** 로또 번호 자동 기입을 시작합니다. **")
-for i in range(1, 6):
-    result = random.sample(nums,5)
-    result.sort()
-    print("%d번째 자동 기입 ==>" % i, *result)
+print()
+print(num_ls)
+print("Average: %.1f" % (sum(num_ls)/len(num_ls)))
+print("Maximum: %.1f" % max(num_ls))
+print("Minimum: %.1f" % min(num_ls))
