978b75283fe75041a38e58a9b4ed6353a95ba328
Woocheol Cho <59816572+inrap8206@users.noreply.github.com>
2022-03-16 04:46:44 +0900

    Create 10_OOP.py

---
+++ 10_OOP.py
@@ -0,0 +1,16 @@
+'''
+Object-Oriented Programming : 객체지향 프로그래밍
+객체란 모종의 변수와 메서드를 담는 추상적 개념
+python은 모든것이 객체임  ex) list, string, int, function
+
+Class : 데이터와 함수를 묶어 객체로 만들어 주는 개념. 객체의 기반이 되는 틀을 정의함.
+'''
+
+# Class의 구성요소 : 변수와 메서드
+class Dog:
+  name = 'coco'     # 변수
+  
+  def bark(self):   # 메서드
+    return 'wal wa'
+  
+
