3dd6ec0f7bf7ffddee9510bdb2447a3d1dce762c
Woocheol Cho <59816572+inrap8206@users.noreply.github.com>
2022-03-16 04:23:22 +0900

    Update 02_list.py

--- 02_list.py
+++ 02_list.py
@@ -71,7 +71,24 @@ a = [1, 7, 9, 2, 5]
 a.reverse()       # [5, 2, 9, 7, 1]
 
 
+# 최대, 최소, 합
 a = [45, 87, 99, 21, 55]
 max(a)   # 리스트 내 최대값 요소 99
 min(a)   # 리스트 내 최소값 요소 21
 sum(a)   # 리스트 내 요소값 합  307
+
+
+# List Comprehensions
+# [표현식 for 요소 in 시퀀스자료형 [if 조건식]]
+[i for i in range(1,6)]     # [1, 2, 3, 4, 5]
+[i for i in range(1,6) if i % 2 == 0]
+[x * y for x in L1 for y in L2]
+
+
+# enumerate()     # 리스트의 모든 요소를 인덱스와 쌍으로 추출
+for idx, color in enumerate(colors): 
+  print(idx, color)         # idx=0, color='red'  ...
+  
+  
+# zip()     # 2개 이상의 리스트를 각 리스트의 같은 인덱스 원소끼리 묶은 튜플을 요소로 하는 리스트로 반환
+list(zip([1,2,3], [4,5,6], ['a','b','c']))    # [(1, 4, 'a'), (2, 5, 'b'), (3, 6, 'c')]
